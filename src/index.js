f670973f7a02d3bb4a5b6903319e1924bb828196
Анастасия <nastyaberezyuk8@gmail.com>
2018-09-26 23:14:02 +0300

    com1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var D, a, b, c, solutions,x1,x2;
+   var str=equation.split(' ');
+    a=parseInt(str[0]);
+    b=parseInt(str[4]);
+    c=parseInt(str[8]);
+    if(str[3]=='-')
+        b= -b;
+    if(str[7]=='-')
+        c= -c;
+    D=b*b-4*a*c;
+    x1=((-b-Math.sqrt(D))/(2*a));
+   //Math.round(x1);
+    x1=parseInt(x1.toFixed(0));
+    x2=((-b+Math.sqrt(D))/(2*a));
+//Math.round(x2);
+   x2=parseInt( x2.toFixed(0));
+    if (x1>x2)
+    solutions=[x2,x1];
+    //Math.ceil(solutions);
+    else
+         solutions=[x1,x2];
+    return solutions;
+        
 }
